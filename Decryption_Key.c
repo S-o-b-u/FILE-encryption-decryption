803cc314cd492008dcfce3a2e3bf00d182288286
S-o-b-u <138811082+S-o-b-u@users.noreply.github.com>
2023-08-07 23:58:44 +0530

    Add files via upload

---
+++ Decryption_Key.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// C program to encrypt a file.
+
+int main()
+{
+	char inputfile[20], ch;
+	FILE *fptr1, *fptr2;
+	
+	printf("Enter Your <DATA FILE> Name :\n");
+	scanf("%s", inputfile);
+	
+	fptr1 = fopen(inputfile, "r");
+	if(fptr1 == NULL)
+	{
+		printf("Error Read File\n");
+		printf("Try Again Later :(");
+		exit(0);
+	}
+	
+	fptr2 = fopen("encrypted.txt", "w");
+	if(fptr2 == NULL)
+	{
+		printf("Error Write File\n");
+		printf("Try Again Later :(");
+		exit(0);
+	}
+	
+		ch = fgetc(fptr1);
+		
+		while(ch!=EOF){
+			fputc(ch-3, fptr2);
+				ch = fgetc(fptr1);
+		}
+	
+	fclose(fptr1);
+	fclose(fptr2);
+	printf("Thank You For Using Me >< \n");
+	printf("Your <ENCRYPTED FILE> Is Ready :)\n");
+	printf("Have A Great Day <==>");
+	return 0;
+}
